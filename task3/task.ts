b51ca7b8e0ad69e65a1354cbcd0b2933d73f65f1
Павел Шавленков <69006300+shavlenkov@users.noreply.github.com>
2022-05-03 11:35:16 +0300

    level-3.1 (task3)

--- task3/task.ts
+++ task3/task.ts
@@ -6,12 +6,12 @@ interface B {
     cvalue: undefined | number | string | A
 }
 
-function summ(a: Obj) {
+function summ(a: A) {
     const x = Object.keys(a).map((k) => {
         const elem = a[k];
-        if (typeof elem === 'undefined') return 2022;
-        if (typeof elem.cvalue === 'string') return +elem.cvalue || 2022;
-        if (typeof elem.cvalue === 'object') return summ(elem.cvalue);
+        if (typeof elem?.cvalue === 'undefined') return 2022;
+        if (typeof elem?.cvalue === 'string') return +elem.cvalue || 2022;
+        if (typeof elem?.cvalue === 'object') return summ(elem.cvalue);
         return elem.cvalue;
     });
 
